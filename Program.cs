421904f2545e86598c26cdffaece5a3505278718
Anthony Sneed <tony@tonysneed.com>
2019-02-10 14:52:22 -0600

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_vs_live_share
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
